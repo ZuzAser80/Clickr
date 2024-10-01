4d860673c959e7067982120011da382a9bb6066e
ZuzAser80 <dde4th@gmail.com>
2024-10-01 19:46:45 +0500

    Update AI.cs

--- Assets/Scripts/Player/AI.cs
+++ Assets/Scripts/Player/AI.cs
@@ -7,6 +7,7 @@ public class AI : ISP {
 
     private float m = 0;
     private float n = 0;
+    private float d = 0;
 
     public void PutOnCooldown() {
         StartCoroutine(wait(
@@ -14,7 +15,7 @@ public class AI : ISP {
             delegate { timer += MathF.Round(Time.deltaTime / 4f, 3); },
             4f
         ));
-        StartCoroutine(tryShoot(2.5f + m));
+        StartCoroutine("tryShoot", 2f + m);
     }
 
     public override void OnStartAuthority()
@@ -35,17 +36,24 @@ public class AI : ISP {
 
     public override void CmdClick()
     {
-        if(n < .15f) { return; }
+        if(n < 0.15f) { return; }
         n = 0;
+        //d = 0;
         base.CmdClick();
     }
 
+    // public void tryCmdClick() {
+    //     if(n < 2f) { return; }
+    //     n = 0;
+    //     CmdClick();
+    // }
+
     public IEnumerator tryShoot(float seconds) {
-        if(c == 0) { StopCoroutine(tryShoot(seconds)); StartCoroutine(tryShoot(seconds)); yield return null; }
+        if(c == 0) { StopCoroutine("tryShoot"); StartCoroutine("tryShoot", 2f + m); yield return null; }
         yield return new WaitForSeconds(seconds);
         CmdClick();
-        StopCoroutine(tryShoot(seconds));
-        StartCoroutine(tryShoot(seconds));
+        StopCoroutine("tryShoot");
+        StartCoroutine("tryShoot", 2f + m);
     }
 
     public IEnumerator wait(Action action, Action update, float seconds) {
@@ -59,5 +67,6 @@ public class AI : ISP {
 
     private void Update() {
         n+=Time.deltaTime;
+        d += Time.deltaTime;
     }
 }
