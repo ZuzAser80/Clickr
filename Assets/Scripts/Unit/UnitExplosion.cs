14fdd6dfa89c4422cd4098e37d979abce5959bac
ZuzAser80 <dde4th@gmail.com>
2024-10-12 19:43:10 +0500

    m

--- Assets/Scripts/Unit/UnitExplosion.cs
+++ Assets/Scripts/Unit/UnitExplosion.cs
@@ -8,15 +8,15 @@ public class UnitExplosion : MonoBehaviour {
     private ParticleSystem sys;
     
     private void Start() {
-        GetComponent<Renderer>().material.color = Color.white;
+        //GetComponent<Renderer>().material.color = Color.white;
         sys = GetComponent<ParticleSystem>();
         transform.localScale *= rad*2;
         StartCoroutine(clr());
     }
 
     private IEnumerator clr() {
-        if(a < -50) { if(!sys.isPlaying) {sys.Play();} else { Destroy(this, 2); yield return null; }}
-        GetComponent<Renderer>().material.color = new Color(255, 255, 255, a);
+        if(a < 0) { if(!sys.isPlaying) {sys.Play();} else { Destroy(this, 8); yield return null; }}
+        GetComponent<Renderer>().material.color = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, a);
         a -= 25f;
         yield return new WaitForSeconds(1f);
         StartCoroutine(clr());
